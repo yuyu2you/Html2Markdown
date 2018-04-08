c93482881cb49b7216ff60f5369bfedf3ecbf897
Simon Baynes <baynezy@gmail.com>
2018-04-08 08:21:30 +0100

    Revert "Add OpenCover coverage build"
    This reverts commit 3aaa061ff2800d19efa6c0d8897185b4e48ee81d.

--- GenerateCodeCoverage.bat
+++
@@ -1,3 +0,0 @@
-opencover.console -register:user "-target:C:\Program Files\dotnet\dotnet.exe" "-targetargs:test .\test\Html2Markdown.Test\Html2Markdown.Test.csproj" -filter:+[Html2Markdown.Test*]* -output:coverage.xml -oldStyle
-
-pause
